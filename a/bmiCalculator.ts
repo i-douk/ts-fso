64cf9c7d9a8904d5928bee2353e640d5184d053b
ihssandk <ihssandoukali@gmail.com>
2024-05-07 19:39:48 +0100

    9.5

--- a/bmiCalculator.ts
+++ a/bmiCalculator.ts
@@ -1,54 +1,47 @@
 interface BodyParams {
     height: number;
     weight: number;
-  }
-  
-  const parseArguments = (args: string[]): BodyParams => {
+}
+
+const parseArguments = (args: string[]): BodyParams => {
     if (args.length < 4) throw new Error('Not enough arguments');
     if (args.length > 4) throw new Error('Too many arguments');
-  
-    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
-      return {
-        height: Number(args[2]),
-        weight: Number(args[3])
-      }
-    }else {
-      throw new Error('Provided values were not numbers!');
-    }
 
-  }
-  
-  const calculateBmi = (a: number, b: number) : string => {
-    const bmi = b / (a *0.01)**2
-    switch(true) {
-      case (bmi < 16.0) :
-        return 'Underweight (Severe thinness) '
-      case (bmi > 16.0 && bmi < 16.9):
-        return 'Underweight (Moderate thinness)';
-      case (bmi > 17.0 && bmi < 18.4):
-        return 'Underweight (Mild thinness';
-      case (bmi > 18.5  && bmi < 24.9 ):
-        return 'Normal (healthy weight)';
-      case (bmi > 25.0 && bmi < 29.9):
-        return 'Overweight (Pre-obese)';
-      case (bmi > 30.0 && bmi < 34.9):
-        return 'Obese (Class I) ';
-      case (bmi > 35.0   && bmi < 39.9):
-        return 'Obese (Class II) ';
-      case (bmi >= 40 ):
-        return 'Obese (Class III) ';
-      default:
-        throw new Error('Check if you have provided height in cm and weight in kg');
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+        return {
+            height: Number(args[2]),
+            weight: Number(args[3])
+        };
+    } else {
+        throw new Error('Provided values were not numbers!');
     }
-  }
-  
-  try {
-    const { height, weight } = parseArguments(process.argv);
-    console.log(calculateBmi(height, weight));
-  } catch (error: unknown) {
-    let errorMessage = 'Something bad happened.'
-    if (error instanceof Error) {
-      errorMessage += ' Error: ' + error.message;
+};
+
+const calculateBmi = (a: number, b: number): string => {
+    const bmi = b / (a * 0.01) ** 2;
+    switch (true) {
+        case bmi < 16.0:
+            return 'Underweight (Severe thinness)';
+        case bmi > 16.0 && bmi < 16.9:
+            return 'Underweight (Moderate thinness)';
+        case bmi > 17.0 && bmi < 18.4:
+            return 'Underweight (Mild thinness)';
+        case bmi > 18.5 && bmi < 24.9:
+            return 'Normal (healthy weight)';
+        case bmi > 25.0 && bmi < 29.9:
+            return 'Overweight (Pre-obese)';
+        case bmi > 30.0 && bmi < 34.9:
+            return 'Obese (Class I)';
+        case bmi > 35.0 && bmi < 39.9:
+            return 'Obese (Class II)';
+        case bmi >= 40:
+            return 'Obese (Class III)';
+        default:
+            throw new Error('Check if you have provided height in cm and weight in kg');
     }
-    console.log(errorMessage);
-  }
+};
+
+export default  {
+    parseArguments,
+    calculateBmi
+};
--- a/index.ts
+++ a/index.ts
@@ -1,8 +1,31 @@
 import express from 'express';
+import  bmiCalculator   from './bmiCalculator';
+import { isNotNumber } from "./utils";
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack');
+});
+app.get(`/bmi`, (req, res) => {
+    try {
+        const { height , weight} = req.query;
+        if(!isNotNumber(height) && !isNotNumber(weight)) {
+            const bmiResult = bmiCalculator.calculateBmi(Number(height), Number(weight))
+            res.send({
+                height: Number(height),
+                weight: Number(weight),
+                bmi: bmiResult
+            });  
+        } else {
+            res.status(400).send("Height and weight must be numbers");
+        }
+    } catch (error) {
+        let errorMessage = 'something went wrong';
+        if (error instanceof Error) {
+            errorMessage += '. Error: ' + error.message;
+        }
+        res.status(500).send(errorMessage);
+    }
 });
 
 const PORT = 3003;
