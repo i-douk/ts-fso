2dd0303d37da077d54b0201e0820cf12062becb5
ihssandk <ihssandoukali@gmail.com>
2024-05-11 00:28:15 +0100

    9.15

--- part9-d/src/components/Content.tsx
+++ part9-d/src/components/Content.tsx
@@ -1,15 +1,78 @@
-interface CourseInfo { 
-    courseParts: { 
-        name: string;
-        exerciseCount: number; }[]; 
+ interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
 }
-const Content = (props : CourseInfo) => {
-    console.log(props.courseParts)
+
+  interface CoursePartBasic extends CoursePartBase {
+  description: string;
+  kind: "basic"
+}
+
+ interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number;
+  kind: "group"
+}
+
+ interface CoursePartBackground extends CoursePartBase {
+  description: string;
+  backgroundMaterial: string;
+  kind: "background"
+}
+
+ interface CoursePartSpecial extends CoursePartBase {
+    description: string;
+    requirements: string[];
+    kind: "special"
+}
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
+interface ContentProps {
+  courseParts: CoursePart[];
+}
+
+export type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground | CoursePartSpecial;
+const Content = (props : ContentProps )  => {
   return (
     <div>
-        {props.courseParts.map((c , index) => <p key={index}>{c.name} {c.exerciseCount}</p>)}
+      {props.courseParts.map((part: CoursePart) => {
+        switch (part.kind) {
+          case 'basic':
+            return (
+              <div key={part.name}>
+                <p><b>{part.name} {part.exerciseCount}</b></p>
+                <p>{part.description}</p>
+              </div>
+            );
+          case 'group':
+            return (
+              <div key={part.name}>
+                <p><b>{part.name} {part.exerciseCount}</b></p>
+                <p>project exercises: {part.groupProjectCount}</p>
+              </div>
+            );
+          case 'background':
+            return (
+              <div key={part.name}>
+                <p><b>{part.name} {part.exerciseCount}</b></p>
+                <p>submit to : {part.backgroundMaterial}</p>
+              </div>
+            );
+          case 'special':
+            return (
+              <div key={part.name}>
+                <p><b>{part.name} {part.exerciseCount}</b></p>
+                <p>required skills : {part.requirements.join(', ')}</p>
+              </div>
+            );
+          default:
+            return assertNever(part);
+        }
+      })}
     </div>
-  )
+  );
 }
 
 export default Content
