62692d60aafa628f1ae2f13d39431178ad5d9780
ihssandk <ihssandoukali@gmail.com>
2024-05-13 11:12:37 +0100

    9.23

--- patientor-frontend/src/components/SinglePatientView/index.tsx
+++ patientor-frontend/src/components/SinglePatientView/index.tsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import {  Patient } from "../../types";
+import {  Entry, Patient } from "../../types";
 import FemaleIcon from '@mui/icons-material/Female';
 import MaleIcon from '@mui/icons-material/Male';
 import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
@@ -21,9 +21,19 @@ const PatientView = ({ patients } : Props ) => {
             { currPatient.gender === 'female' && <FemaleIcon />}
             { currPatient.gender === 'other' && <QuestionMarkIcon />}
             </span>
-        </div>
         <p>ssn : {currPatient.ssn}</p>
         <p>occupation : {currPatient.occupation}</p>
+        <div><b>entries</b></div>   
+                {currPatient.entries.map((e: Entry) => (
+                <>
+                    <div key={e.id}>{e.date} {e.description}</div>
+                    <ul>
+                        {e.diagnosisCodes?.map(c=><li key={c}>{c}</li>)}
+                    </ul>
+                </>
+        ))}
+           
+        </div>
     
     </>
   );
